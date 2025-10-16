0b68fdf17749fc111ce4d9f7fc1d670f034f20c3
Mukesh-Kumar-Mahato <mukeshsingh98121159@gmail.com>
2025-10-16 10:49:08 +0545

    correcting public channel

--- components/public_channel/api/config.php
+++ components/public_channel/api/config.php
@@ -1,15 +1,3 @@
 <?php
-// api/config.php
-
-$servername = "localhost:8081";
-$username = "root";
-$password = "";
-$database = "ioesems";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include '../../login/config.php';
+?>
